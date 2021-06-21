185a65d5e2142cb90b5c7ad3018df4d64b8ace71
N-010 <chelsig96@gmail.com>
2021-06-21 10:07:04 +0300

    Async task

---
+++ Source/LagCompensation/Private/Asyncs/Tasks/AsyncLineWeaponShot.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tasks/AsyncLineWeaponShot.h"
+
+FAsyncLineWeaponShot::FAsyncLineWeaponShot(UObject* WorldContextObject, UPositionStore* InOwnerPositionStore,
+	const FVector& InShootPosition, const FVector& InShootDirection, const float& InMaxDist,
+	ETraceTypeQuery InTraceChannel, bool bInTraceComplex, const float& InPredictionTime)
+		: Super()
+{
+}
---
+++ Source/LagCompensation/Public/Asyncs/Tasks/AsyncLineWeaponShot.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Async/AsyncWork.h"
+
+class UPositionStore;
+
+class FAsyncLineWeaponShot : public FNonAbandonableTask
+{
+	friend class FAutoDeleteAsyncTask<FAsyncLineWeaponShot>;
+	using Super = FNonAbandonableTask;
+
+private:
+	TWeakObjectPtr<UObject> WorldContext;
+	TWeakObjectPtr<UPositionStore> OwnerPositionStore;
+	FVector ShootPosition;
+	FVector ShootDirection;
+	float MaxDist;
+	ETraceTypeQuery TraceChannel;
+	bool bTraceComplex;
+	float PredictionTime;
+
+public:
+	FAsyncLineWeaponShot(UObject* WorldContextObject, UPositionStore* InOwnerPositionStore,
+											const FVector& InShootPosition,
+											const FVector& InShootDirection,
+											const float& InMaxDist, ETraceTypeQuery InTraceChannel,
+											bool bInTraceComplex,
+											const float& InPredictionTime = 0.f);
+};
